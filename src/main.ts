3ad434f914cd605dbe8d2001fb598b9264c4e04f
mrizwanashiq <mrizwanashiq@outlook.com>
2022-11-29 10:22:49 +0500

    making it better

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,4 @@
-import {
-  ClassSerializerInterceptor,
-  HttpStatus,
-  UnprocessableEntityException,
-  ValidationPipe,
-} from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import type { NestExpressApplication } from '@nestjs/platform-express';
 import { ExpressAdapter } from '@nestjs/platform-express';
@@ -16,42 +11,38 @@ import { ConfigrationService } from './configration/configration.service';
 import { ConfigrationModule } from './configration/configration.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule,
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
     new ExpressAdapter(),
     { cors: true },
-    );
-    app.set('trust proxy', 1); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
-    app.use(helmet());
-    // app.setGlobalPrefix('/api'); use api as global prefix if you don't have subdomain
-    app.use(
-      rateLimit({
-        windowMs: 15 * 60 * 1000, // 15 minutes
-        max: 100, // limit each IP to 100 requests per windowMs
-      }),
-    );
-  
+  );
+  app.set('trust proxy', 1); // only if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+  app.use(helmet());
+  // app.setGlobalPrefix('/api'); use api as global prefix if you don't have subdomain
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
 
-    app.enableVersioning();
-   
-  
-    app.useGlobalPipes(
-      new ValidationPipe({
-      }),
-    );
-  
-    const configService = app.select(ConfigrationModule).get(ConfigrationService);
-    if (configService.documentationEnabled) {
-      setupSwagger(app);
-    }
-  
-    app.use(expressCtx);
-  
-    // Starts listening for shutdown hooks
-    if (!configService.isDevelopment) {
-      app.enableShutdownHooks();
-    }
-  
-    const port = configService.appConfig.port;
+  app.enableVersioning();
+
+  app.useGlobalPipes(new ValidationPipe({}));
+
+  const configService = app.select(ConfigrationModule).get(ConfigrationService);
+  if (configService.documentationEnabled) {
+    setupSwagger(app);
+  }
+
+  app.use(expressCtx);
+
+  // Starts listening for shutdown hooks
+  if (!configService.isDevelopment) {
+    app.enableShutdownHooks();
+  }
+
+  const port = configService.appConfig.port;
   await app.listen(port);
   console.info(`server running on ${await app.getUrl()}`);
 
